6d76117d2d1c52325c95c8e63fbfc6913153827d
Tim Dommett <timdom11@gmail.com>
2019-07-24 14:21:08 +0200

    deploying branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This will be Cassandra. 
+          This will be Cassandra.
         </p>
       </header>
     </div>
