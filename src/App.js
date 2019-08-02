f68923cbf0d4f828e0748e9e2984fbde89f97737
Tim Dommett <timdom11@gmail.com>
2019-08-02 10:50:24 +0200

    updated Splash page for developers

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This will be Cassandra.
+        {/* <img src="https://gdurl.com/LBGp" className="App-logo" alt="logo" /> */}
+        <img src="https://gdurl.com/yLkZ" className="heroLogo" alt="logo" />
+        <h1 className="landingTitle">
+          Welcome Volunteer Developers to Cassandra!
+        </h1>
+
+        <p className="landingPara">
+          Join developers from all over the world to make equal access to education a reality for everyone. <a className="landingParaLink" href="https://github.com/the-muses/Cassandra/wiki/Cassandra-Road-Map#mvp-list" target="_blank">Here</a> is a link to our current MVP list and if you need any help then just speak to our chatbot.
         </p>
+
+        {/* <Projects/> */}
       </header>
     </div>
   );
